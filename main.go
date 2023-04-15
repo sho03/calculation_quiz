f3efbc5c3f7e2975b74257e3f6f4da63d5bc0d4e
sho ochiai <ochi1020@gmail.com>
2023-04-15 11:52:53 +0900

    feat: add main function

--- main.go
+++ main.go
@@ -1,7 +1,61 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+)
 
 func main() {
-	fmt.Println("command executed !")
+
+	hard := flag.Bool("hard", false, "if set, a quiz will be hard mode.")
+	flag.Parse()
+
+	var max int
+	if *hard {
+		max = 10000
+	} else {
+		max = 100
+	}
+
+	left := rand.Intn(max + 1)
+	right := rand.Intn(max + 1)
+
+	ops := []string{"+", "-", "*", "/"}
+	randomIndex := rand.Intn(len(ops))
+	op := ops[randomIndex]
+
+	var answer int
+	switch op {
+	case "+":
+		answer = left + right
+	case "-":
+		answer = left - right
+	case "*":
+		answer = left * right
+	case "/":
+		answer = left / right
+	}
+
+	fmt.Println("what is answer ?")
+	fmt.Println(left, op, right)
+
+	sc := bufio.NewScanner(os.Stdin)
+	sc.Scan()
+	inAnswer, err := strconv.Atoi(sc.Text())
+
+	if err != nil {
+		fmt.Println("input number !")
+		return
+	}
+
+	if answer == inAnswer {
+		fmt.Println("You're collect!")
+	} else {
+		message := fmt.Sprintf("You're wrong! collect answer is %d", answer)
+		fmt.Println(message)
+	}
 }
